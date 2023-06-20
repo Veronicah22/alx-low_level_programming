190b8b181892dfd5a274eb3006763180f4667df5
Veronicah22 <naitamanyveronicah@gmail.com>
2023-06-19 17:23:47 -0700

    last dgit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,26 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Description: print 0-9 using putchar
- *              while using int variable
+ * main - Prints numbers from 0 to 9.
  *
  * Return: Always 0 (Success)
-*/
-
+ */
 int main(void)
 {
-	int digit = 0;
+	int i;
 
-	while (digit <= 9)
+	for (i = 48; i < 58; i++)
 	{
-		/convert digit to ASCII representation/
-		putchar(digit + '0');
-		++digit;
+		putchar(i);
 	}
 	putchar('\n');
-
 	return (0);
 }
 
