ef8f1fef2dada22e7647cbceb88c4f4d991b1fbc
Veronicah22 <naitamanyveronicah@gmail.com>
2023-06-23 17:13:57 -0700

    functions

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,17 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes a character to the standard output (stdout)
- * @c: The character to be written
- *
- * Return: On success, returns the number of characters written.
- * On error, returns -1 and sets errno appropriately.
+ * print_numbers - print 0-9
+ * return: void
  */
-int _putchar(char c)
+
+void print_numbers(void)
 {
-return write(1, &c, 1);
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+_putchar(c);
 }
+_putchar('\n');
+}
+
